d46c31b0713cb0f5a635e4e24b17b813359d4ee6
alice23876 <20243ds160@utez.edu.mx>
2025-09-11 20:05:06 -0600

    Agregue class worker con definicion de variables

--- ListTest/src/Main.java
+++ ListTest/src/Main.java
@@ -1,15 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+
+class Worker {
+    int id;
+    String nombre;
+    double sueldo;
+
+    public Worker(int id, String nombre, double sueldo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return "id =" + id + ", nombre =" + nombre + ", sueldo =" + sueldo ;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Lista worker
+        List<Worker> workers = new ArrayList<>();
+        workers.add(new Worker(1, "Marlen", 500));
+        workers.add(new Worker(2, "Sundown", 200));
+        workers.add(new Worker(3, "Emilio", 1760));
+        workers.add(new Worker(4, "Campos", 3500));
+        workers.add(new Worker(5, "Toti", 1500));
+        workers.add(new Worker(6, "Alice", 900));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Lista normal
+        System.out.println("Lista original:");
+        for (Worker w : workers) {
+            System.out.println(w);
+        }
+
+        // Calcular promedio
+        double suma = 0;
+        for (Worker w : workers) {
+            suma += w.sueldo;
+        }
+        double promedio = suma / workers.size();
+        System.out.println("\n Promedio de sueldos: " + promedio);
+
+        // Eliminacion de sueldos
+        workers.removeIf(w -> w.sueldo < 1000);
+
+        // nueva lista
+        System.out.println("Nueva lista mayores a 1000: ");
+        for (Worker w : workers) {
+            System.out.println(w);
         }
     }
 }
