bffe121e64ae39329e1cd6a7bdb5dd5541e1c45a
alice23876 <20243ds160@utez.edu.mx>
2025-10-10 08:55:01 -0600

    Hice un arreglo donde se suman todos los valores y se guarda en suma

---
+++ sumatoriaArreglo/src/Sumatoria.java
@@ -0,0 +1,21 @@
+public class Sumatoria {
+        public static void main(String[] args) {
+            int[][] arrTest = {
+                    {1, 3, 7, 8},
+                    {0, 6, 3, 1},
+                    {1, 2, 3, 4},
+                    {0, 5, 3, 0},
+                    {2, 5, 9, 0}
+            };
+
+            int suma = 0;
+
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 4; j++) {
+                    suma = suma + arrTest[i][j];
+                }
+            }
+
+            System.out.println("La sumatoria es: " + suma);
+        }
+}
