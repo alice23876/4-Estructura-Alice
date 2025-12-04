836bd852eec86e1035af324148240fe7efaf3357
alice23876 <20243ds160@utez.edu.mx>
2025-12-04 07:53:48 -0600

    Subi apunte de BubbleSort

---
+++ BubbleSort/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BubbleSort bubllesort = new BubbleSort();
+        int[] arr = {1, 5, 9, 10, 3, -2, 0, 6, 92, -100};
+        bubllesort.sort(arr);
+    }
+}
+
