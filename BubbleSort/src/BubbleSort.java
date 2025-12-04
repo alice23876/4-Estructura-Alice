d85a02f2c5b8bf96fab99748dc59c0133e994135
alice23876 <20243ds160@utez.edu.mx>
2025-12-04 08:18:26 -0600

    Subi apunte de BubbleSort

--- BubbleSort/src/BubbleSort.java
+++ BubbleSort/src/BubbleSort.java
@@ -2,14 +2,14 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public void sort(int[] arr) {
-        int n = arr.length;
+        int x = arr.length;
         Boolean swapped = false;
 
         //Recorremos todos los elementos del arreglo
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < x - 1; i++) {
 
             //Recorremos los elementos adyacentes -1 -i (Por los que ya fueron recorridos)
-            for (int j = 0; j < n - 1 - i; j++) {
+            for (int j = 0; j < x - 1 - i; j++) {
                 //Pregunta si el elemento corriente es mayor que el siguiente
                 if (arr[j] > arr[j + 1]) {
                     //Se realiza el intercambio
--- BubbleSort/src/Main.java
+++ BubbleSort/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BubbleSort bubllesort = new BubbleSort();
-        int[] arr = {1, 5, 9, 10, 3, -2, 0, 6, 92, -100};
+        int[] arr = {1, 5, 9, 10, 3, -2, 0, 6, 92, -10};
         bubllesort.sort(arr);
     }
 }
